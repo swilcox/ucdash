3d41f3170c06d84d79ac04cc91cbde1bbe1130ec
Steven Wilcox <steven@wilcoxzone.com>
2010-11-30 12:34:14 -0600

    renamed tasks to jobs and simplified the jobs related models

--- ucdash/jobs/models.py
+++ ucdash/jobs/models.py
@@ -1,22 +1,10 @@
 from django.db import models
 
 
-STATUS_CHOICES = [('','Unknown'),
-                  ('A','Okay'),
-                  ('C','Complete'),
-                  ('X','Error'),
-                  ('W','Warning'),
-                  ('L','Late'),
-                  ('M','Missing'),
-                  ('R','Running')]
-
-
 class Job(models.Model):
     name = models.CharField(max_length=50)
     slug = models.SlugField()
     description = models.TextField()
-    options = models.TextField()
-    latest_status = models.CharField(max_length=1,choices=STATUS_CHOICES,default='',blank=True)
 
     def __unicode__(self):
         return str(self.name)
@@ -34,15 +22,15 @@ class JobGroup(models.Model):
 class Notification(models.Model):
     result_code = models.IntegerField()
     job = models.ForeignKey(Job)
-    at = models.DateTimeField(auto_now_add=True,null=True,blank=True)
+    at = models.DateTimeField(auto_now_add=True,null=True,blank=True,db_index=True)
     start_time = models.DateTimeField(null=True,blank=True)
     end_time = models.DateTimeField(null=True,blank=True)
-    run_time = models.BigIntegerField(null=True,blank=True)
-    output_text = models.TextField()
-    errout_text = models.TextField()
+    run_time = models.IntegerField(null=True,blank=True)
+    output_text = models.TextField(blank=True)
+    errout_text = models.TextField(blank=True)
 
     def __unicode__(self):
-        return self.name
+        return 'result: %s for job: %s' % (str(self.result_code), str(self.job))
 
 
 class NotificationExtra(models.Model):
