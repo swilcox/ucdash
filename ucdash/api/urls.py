c2fc90c8716a01cf783f56b2d079dc746a8cff76
Steven Wilcox <steven@wilcoxzone.com>
2012-10-12 19:57:02 -0500

    killed off all the old crap... time to start fresh

--- ucdash/api/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from piston.resource import Resource
-from handlers import JobNotificationHandler
-
-jobnotification_handler = Resource(JobNotificationHandler)
-
-urlpatterns = patterns('',
-   url(r'^notify/(?P<job_slug>[^/]+)/', jobnotification_handler, name='job_notify'),
-)
